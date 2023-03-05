109109d2a91f1327b287e2a32bc02819ba4b7cb2
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-03-05 09:02:55 -0800

    final commit of working code.  test code in freeCodeCamp replit

--- shape_calculator.py
+++ shape_calculator.py
@@ -3,48 +3,36 @@ class Rectangle:
         self.width = width
         self.height = height
 
-
     def __repr__(self):
         return f'Rectangle(width={self.width}, height={self.height})'
-
 
     def set_width(self, new_width):
         self.width = new_width
 
-
     def set_height(self, new_height):
         self.height = new_height
-
 
     def get_area(self):
         area = self.width * self.height
         return area
 
-
     def get_perimeter(self):
         perimeter = self.width * 2 + self.height * 2
         return perimeter
     
-
     def get_diagonal(self):
         diagonal = ((self.width ** 2 + self.height ** 2) ** .5)
         return diagonal
     
-
     def get_picture(self):
         if self.width > 50 or self.height > 50:
             return "Too big for picture."
         rectangle_pic = ("*" * self.width + "\n") * self.height
         return rectangle_pic
-        
-    
+            
     def get_amount_inside(self, shape): 
-        floor_width = self.width // shape.width
-        floor_height = self.height // self. width
-        return floor_width * floor_height
-    
-    #Takes another shape (square or rectangle) as an argument. Returns the number of times the passed in shape could fit inside the shape (with no rotations). For instance, a rectangle with a width of 4 and a height of 8 could fit in two squares with sides of 4.
-
+        return int(self.get_area() / shape.get_area())
+           
 
 class Square(Rectangle):
     def __init__(self, side):
@@ -52,39 +40,15 @@ class Square(Rectangle):
         self.width = side
         self.height = side
 
-
     def __repr__(self):
         return f'Square(side={self.width})'
-
 
     def set_side(self, new_side):
         self.width = new_side
         self.height = new_side
 
-
     def set_width(self, new_side):
         self.width = new_side
 
-
     def set_height(self, new_side):
         self.height = new_side
-
-
-
-# rect = Rectangle(5, 10)
-# print(rect.get_area())
-# rect.set_width(3)
-# rect.set_height(6)
-# print(rect.get_perimeter())
-# print(rect.get_diagonal())
-# print(rect)
-# print(rect.get_picture())
-
-
-# sq = Square(9)
-# print(sq.get_area())
-# sq.set_side(4)
-# print(sq.get_perimeter())
-# print(sq.get_diagonal())
-# print(sq)
-# print(sq.get_picture())
